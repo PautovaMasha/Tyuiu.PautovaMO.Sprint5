c1deee530526e6e50e85629b91d5779b6a00ce31
PautovaMO <Pautova812007@mail.ru>
2025-11-23 19:27:53 +0500

    Task5

---
+++ Tyuiu.PautovaMO.Sprint5.Task5.V11.Lib/DataService.cs
@@ -0,0 +1,33 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint5;
+namespace Tyuiu.PautovaMO.Sprint5.Task5.V11.Lib
+{
+    public class DataService : ISprint5Task5V11
+    {
+        public double LoadFromDataFile(string path)
+        {
+            string strX = File.ReadAllText(path);
+            string[] numbers = strX.Split(' ', ',', '\t', '\n', '\r');
+
+            long product = 1;
+            bool foundOdd = false;
+
+            foreach (string numStr in numbers)
+            {
+                if (int.TryParse(numStr, out int num) && num % 2 != 0)
+                {
+                    product *= num;
+                    foundOdd = true;
+                }
+            }
+
+            if (foundOdd)
+            {
+                return product;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+    }
+}
---
+++ Tyuiu.PautovaMO.Sprint5.Task5.V11/Program.cs
@@ -0,0 +1,44 @@
+﻿
+using System;
+using System.IO;
+using Tyuiu.PautovaMO.Sprint5.Task5.V11.Lib;
+
+namespace Tyuiu.PautovaMO.Sprint5.Task5.V11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #5 | Выполнил: Паутова М.О. | ИБКСб-25-1";
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #5                                                               *");
+            Console.WriteLine("* Тема: Чтение данных из текстового файла                                 *");
+            Console.WriteLine("* Задание #5                                                              *");
+            Console.WriteLine("* Вариант #11                                                             *");
+            Console.WriteLine("* Выполнил: Паутова Мария Олеговна | ИБКСб-25-1                           *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Дан файл, в котором есть набор значений. Найти произведение всех        *");
+            Console.WriteLine("* нечетных целых чисел в файле. У вещественных значений округлить до      *");
+            Console.WriteLine("* трёх знаков после запятой.                                              *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            string path = @"C:\DataSprint5\InPutDataFileTask5V11.txt";
+            Console.WriteLine("Данные находятся в файле: " + path);
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            double result = ds.LoadFromDataFile(path);
+            Console.WriteLine("Произведение нечетных целых чисел: " + result);
+
+            Console.ReadKey();
+        }
+    }
+}
